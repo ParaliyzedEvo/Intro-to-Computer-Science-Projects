b3de5974755417ba02994ef3272f63efb2c724a0
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2025-01-20 18:05:04 -0600

    Delete Andrei's Bowl Shop directory

--- Andrei's Bowl Shop/Module1.vb
+++
@@ -1,6 +0,0 @@
-﻿Module Module1
-    'Define varibles for the entire code
-    Public dblPreTaxCost As Double
-    Public dblTax As Double
-    Public dblFinalCost As Double
-End Module
--- Andrei's Bowl Shop/frmEnter.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
-        Me.Visible = False
-        frmWelcome.Visible = True
-    End Sub
-End Class
